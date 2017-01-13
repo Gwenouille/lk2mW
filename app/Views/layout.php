87a07e268302ea39f533e8980c9281814d6c725c
Gwenouille <gwenael.le-page@orange.fr>
2017-01-13 12:01:18 +0100

    Correction du CSS pour la page d'accueil: liens partenaires en bas, espacement du cercle de lien, apparition/disparition de la nav

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -10,6 +10,8 @@
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/localBootstrap.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/master.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/header_footer.css') ?>">
+	<!-- PERMET D'AJOUTER DES CSS ICI -->
+	<?= $this->section('css') ?>
 	<title><?= $this->e($title) ?></title>
 </head>
 <body>
@@ -39,7 +41,7 @@
 			</div>
 		</nav>
 
-		<section>
+		<section class="section">
 			<?= $this->section('main_content') ?>
 		</section>
 
@@ -55,8 +57,12 @@
 			</address>
 		</footer>
 	</div>
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+	<!-- PERMET D'AJOUTER DES JS ICI -->
+	<?= $this->section('js') ?>
+
 </body>
 </html>
