73efc02a3073004eff3b61ccb827899471a20e19
Mekabulle <mekabulle@users.noreply.github.com>
2017-01-22 23:03:40 +0100

    Design de mon compte et mes projets

--- app/Views/user/UserView.php
+++ app/Views/user/UserView.php
@@ -7,57 +7,45 @@
 
 <?php $this->start('main_content'); ?>
 
-<!--navigation ckeckbox user-->
-<div class="userCheckbox">
-	<!-- POURQUOI DES INPUT ? -->
-	<!-- Des input pour faire joli, gloussa Poulette -->
-<!-- <label>
-  <input type="checkbox" id="cbox1" value="checkbox1">
-  MON COMPTE
-</label>
-<label>
-  <input type="checkbox" id="cbox2" value="checkbox2">
-  MES PROJETS
-</label> -->
-<a href="<?= $this->url("user_home"); ?>">MON COMPTE</a>
-<a href="<?= $this->url("projects_home"); ?>">MES PROJETS</a>
+<div class="userContent">
+<!--navigation  choix de l'user-->
+<div class="userChoice">
+<a href="<?= $this->url("user_home"); ?>"><span class="glyphicon glyphicon-user"></span>MON COMPTE</a>&nbsp;&nbsp;
+<a href="<?= $this->url("projects_home"); ?>"><span class="glyphicon glyphicon-list-alt"></span>MES PROJETS</a>
 </div>
-
-<?php if (isset($errorModifyCoordinates)){
-	echo ('<h2>Probleme lors de la mise à jour de vos coordonnées. Veuillez réessayer</h2>');
-}?>
 
 <!--Formulaire compte utilisateur  -->
 <div class="main-login_connexion">
-	<form class="form-inscription" method="post" action="<?= $this->url("user_modifyCoordinates"); ?>">
-		<h3 class="form_section center">Mes données utilisateur</h3>
+	<form class="form-inscription" method="post" action="<?= $this->url("user_signin"); ?>">
+		<h3 class="form_section center">Gestion de mes données utilisateur</h3>
 
 		<span class="asterix obligatoire center">* Champs Obligatoires</span>
 
 		<label for="lastname">Votre Nom<span class="asterix">*</span> : </label>
-		<input type="text" name="lastname" placeholder="Nom" value="<?= $_SESSION['user']['lastname']?>" required>
+		<input type="text" name="lastname" placeholder="Nom" required>
 
 		<label for="firstname">Votre Prénom<span class="asterix">*</span> : </label>
-		<input type="text" name="firstname" placeholder="Prénom" value="<?= $_SESSION['user']['firstname']?>" required>
+		<input type="text" name="firstname" placeholder="Prénom" required>
 
 		<label for="email">Votre E-mail<span class="asterix">*</span> : </label>
-		<input type="email" name="email" placeholder="E-Mail" value="<?= $_SESSION['user']['mail']?>" required>
+		<input type="email" name="email" placeholder="E-Mail" required>
 
 		<label for="numTel">Votre Numéro de Téléphone : </label>
-		<input type="text" name="numTel" placeholder="Téléphone (Optionnel)" value="<?= $_SESSION['user']['phone']?>">
+		<input type="text" name="numTel" placeholder="Téléphone (Optionnel)">
 
-		<input type="hidden" name="form_name" value="modifyCoordinates">
+		<input type="hidden" name="form_name" value="signIn">
 		<input class="input-submit" type="submit" name="inscription" value="Modifier">
 	</form>
 </div>
-
-<!-- COMMENTÉ CAR PAS A SA PLACE ? -->
+</div>
 <!--bloc des projets utilisateurs  -->
 	<!-- <div>
 	<h3>Mes projets</h3>
 			<button type="button" name="createProject">Créer un projet</button>
 			<button type="button" name="openProject">Ouvrir un projet</button>
 </div> -->
+
+
 
 
 <?php $this->stop('main_content') ?>
