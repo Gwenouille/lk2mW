8433ad1416ea62e7b1ee7b67568bd328190f0c22
Gwenouille <gwenael.le-page@orange.fr>
2017-01-23 10:53:24 +0100

    Correction du UserView.php pour l'affichage par defaut des valeurs passé le config.php dans le gitignore

--- app/Views/user/UserView.php
+++ app/Views/user/UserView.php
@@ -8,54 +8,45 @@
 <?php $this->start('main_content'); ?>
 
 <div class="userContent">
-<!--navigation  choix de l'user-->
-<div class="userChoice">
-<a href="<?= $this->url("user_home"); ?>"><span class="glyphicon glyphicon-user"></span>MON COMPTE</a>&nbsp;&nbsp;
-<a href="<?= $this->url("projects_home"); ?>"><span class="glyphicon glyphicon-list-alt"></span>MES PROJETS</a>
+	<!--navigation  choix de l'user-->
+	<div class="userChoice">
+	<a href="<?= $this->url("user_home"); ?>"><span class="glyphicon glyphicon-user"></span>MON COMPTE</a>&nbsp;&nbsp;
+	<a href="<?= $this->url("projects_home"); ?>"><span class="glyphicon glyphicon-list-alt"></span>MES PROJETS</a>
+	</div>
+
+	<?php if (isset($errorModifyCoordinates)){
+		$feedback='Problème lors de la mise à jour de vos coordonnées. Veuillez réessayer.';
+	} else {
+		$feedback='';
+	}?>
+
+	<!--Formulaire compte utilisateur  -->
+	<div class="main-login_connexion">
+		<form class="form-inscription" method="post" action="<?= $this->url("user_modifyCoordinates"); ?>">
+		 	<h3 class="form_section center">Mes données utilisateur</h3>
+
+			<span class="asterix obligatoire center">* Champs Obligatoires</span>
+
+			<label for="lastname">Votre Nom<span class="asterix">*</span> : </label>
+			<input type="text" name="lastname" placeholder="Nom" value="<?= $_SESSION['user']['lastname']?>" required>
+
+			<label for="firstname">Votre Prénom<span class="asterix">*</span> : </label>
+			<input type="text" name="firstname" placeholder="Prénom" value="<?= $_SESSION['user']['firstname']?>" required>
+
+			<label for="email">Votre E-mail<span class="asterix">*</span> : </label>
+			<input type="email" name="email" placeholder="E-Mail" value="<?= $_SESSION['user']['mail']?>" required>
+
+			<label for="numTel">Votre Numéro de Téléphone : </label>
+			<input type="text" name="numTel" placeholder="Téléphone (Optionnel)" value="<?= $_SESSION['user']['phone']?>">
+
+			<input type="hidden" name="form_name" value="modifyCoordinates">
+			<input class="input-submit" type="submit" name="inscription" value="Modifier">
+
+			<p class="center">
+				<?=$feedback?>
+			</p>
+		</form>
+	</div>
 </div>
-
-<?php if (isset($errorModifyCoordinates)){
-	$feedback='Problème lors de la mise à jour de vos coordonnées. Veuillez réessayer.';
-} else {
-	$feedback='';
-}?>
-
-<!--Formulaire compte utilisateur  -->
-<div class="main-login_connexion">
-	<form class="form-inscription" method="post" action="<?= $this->url("user_signin"); ?>">
-		<h3 class="form_section center">Gestion de mes données utilisateur</h3>
-
-		<span class="asterix obligatoire center">* Champs Obligatoires</span>
-
-		<label for="lastname">Votre Nom<span class="asterix">*</span> : </label>
-		<input type="text" name="lastname" placeholder="Nom" required>
-
-		<label for="firstname">Votre Prénom<span class="asterix">*</span> : </label>
-		<input type="text" name="firstname" placeholder="Prénom" required>
-
-		<label for="email">Votre E-mail<span class="asterix">*</span> : </label>
-		<input type="email" name="email" placeholder="E-Mail" required>
-
-		<label for="numTel">Votre Numéro de Téléphone : </label>
-		<input type="text" name="numTel" placeholder="Téléphone (Optionnel)">
-
-		<input type="hidden" name="form_name" value="signIn">
-		<input class="input-submit" type="submit" name="inscription" value="Modifier">
-
-		<p class="center">
-			<?=$feedback?>
-		</p>
-	</form>
-</div>
-</div>
-<!--bloc des projets utilisateurs  -->
-	<!-- <div>
-	<h3>Mes projets</h3>
-			<button type="button" name="createProject">Créer un projet</button>
-			<button type="button" name="openProject">Ouvrir un projet</button>
-</div> -->
-
-
-
 
 <?php $this->stop('main_content') ?>
