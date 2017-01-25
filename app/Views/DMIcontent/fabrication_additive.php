3f4db5295168514ee30dd1b9272803817001012c
Gwenouille <gwenael.le-page@orange.fr>
2017-01-25 10:54:15 +0100

    Carousel généré dynamiquement Repassé à Bootstrap 3

--- app/Views/DMIcontent/fabrication_additive.php
+++ app/Views/DMIcontent/fabrication_additive.php
@@ -14,45 +14,59 @@
       <span>Fabrication Additive</span>
     </h2>
 <!-- debut du carousel -->
-    <a href="<?= $this->url("news_home"); ?>">news</a>
+    <a href="<?= $this->url("news_home"); ?>">
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+
       <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      <?php	foreach ($newsList as $key=>$value) {
+    		if ($newsList[$key]['state']==='1'){?>
+
+        <li data-target="#carousel-example-generic" data-slide-to="<?=$key?>" <?php if($key==0){echo("class=active");}?>></li>
+
+    		<?php
+    		}
+    	}?>
       </ol>
+
       <div class="carousel-inner" role="listbox">
-        <div class="carousel-item active">
-          <img src="<?= $this->assetUrl('images/journaux1.jpg'); ?>" alt="First slide">
+      <?php foreach ($newsList as $key=>$value) {
+        if ($newsList[$key]['state']==='1'){?>
+
+        <div class="item <?php if($key==0){echo('active');}?>">
+          <?php
+          // S'il n'y a pas d'image dans cette news, on renvoie l'image noire.
+          if (empty($newsList[$key]['pictures'])){
+            $url="images/news/empty.jpg";
+          } else {
+          $url="images/";
+          $url.=$newsList[$key]['id'];
+          $url.="/";
+          $url.=$newsList[$key]['pictures'][0]['name'];
+          $url.=".";
+          $url.=$newsList[$key]['pictures'][0]['type'];
+          }
+          ?>
+          <img src="<?= $this->assetUrl($url); ?>" alt="First slide">
           <div class="carousel-caption">
-            <h3>1ere news</h3>
-            <p>Ceci est la premiere news</p>
+            <h3><?=$newsList[$key]['title']?></h3>
           </div>
         </div>
-        <div class="carousel-item">
-          <img src="<?= $this->assetUrl('images/journaux2.jpg'); ?>" alt="Second slide">
-          <div class="carousel-caption">
-            <h3>2eme news</h3>
-            <p>Ceci est la deuxieme news</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="<?= $this->assetUrl('images/journaux3.jpg'); ?>" alt="Third slide">
-          <div class="carousel-caption">
-            <h3>3eme news</h3>
-            <p>Ceci est la troisieme news</p>
-          </div>
-        </div>
+
+        <?php
+        }
+      }?>
       </div>
+
       <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-        <span class="icon-prev" aria-hidden="true"></span>
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
       </a>
       <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-        <span class="icon-next" aria-hidden="true"></span>
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
       </a>
     </div>
+    </a>
 <!--  fin du carousel-->
 
     <div class="actu-fabrication">
